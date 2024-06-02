2949c61b312bc55ef1cf51272e72d1b9b11c3173
tech-z <152055911+Achi-Vyshnavi@users.noreply.github.com>
2024-06-02 21:08:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+    // Get the button element
+    var btn = document.getElementById("bg");
+
+    // Add click event listener to the button
+    btn.addEventListener("click", function() {
+        // Redirect to home.html
+        window.location.href = "home.html";
+    });
+
